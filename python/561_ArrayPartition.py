675ac982a3689afbf2e66d8cb9c3120925f72a55
Gina Huang <gina_huang@trendmicro.com>
2023-03-21 16:44:36 -0400

    easy

--- python/561_ArrayPartition.py
+++ python/561_ArrayPartition.py
@@ -1,37 +1,18 @@
 class Solution(object):
     def arrayPairSum(self, nums):
-        all = []
-        self.helper(nums, [], all)
-        print('len', len(all))
-        for a in all:
-            print(a)
-        max_n = -1
-        for a in all:
-            total = sum([min(c) for c in a])
-            max_n = total if total > max_n else max_n
-        return max_n
-
-    def helper(self, nums, current, all):
-        if len(nums) == 0:
-            all.append(current.copy())
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                b = nums.pop(j)
-                a = nums.pop(i)
-                current.append([a, b])
-                self.helper(nums, current, all)
-                nums.insert(i, a)
-                nums.insert(j, b)
-                current.pop()
-
-#            current.append(nums.pop(i))
-#            self.helper(nums, current, all)
-#            nums.insert(i, current.pop())
+        nums.sort(reverse=True)
+        total = 0
+        for i in range(1, len(nums), 2):
+            total += nums[i]
+        return total
 
 
 if __name__ == '__main__':
     solution = Solution()
+#    nums = [5, 1, 3, 4]
 #    nums = [1, 4, 3, 2]
-    nums = [6, 2, 6, 5, 1, 2]
+#    nums = [6, 2, 6, 5, 1, 2]
+    nums = [1, 2, 3, 4, 5, 6]
+#    nums = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ans = solution.arrayPairSum(nums)
     print(ans)
